4213c8dbf2d6c9eb6b6ad40a71c8d2f89c6b4ce6
simon-mos17 <simonmoser1@gmx.de>
2025-10-01 14:42:41 +0200

    Backend initialisiert

---
+++ Backend/src/main/java/com/simon_mos17/ai_taskmanager/AiTaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.simon_mos17.ai_taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTaskmanagerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/simon_mos17/ai_taskmanager/AiTaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon_mos17.ai_taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
